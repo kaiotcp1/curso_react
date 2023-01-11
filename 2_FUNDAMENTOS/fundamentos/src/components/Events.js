61ef71b65f0b1b3e3a6446ae16d4ab7e6a4d1899
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-11 18:26:45 -0300

    Função nos eventos

--- 2_FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -9,6 +9,14 @@ const Events = () => {
             <div>
                 <button onClick={handleMyEvent}>Clique aqui</button>
             </div>
+            <div>
+                <button onClick={() => console.log("Clicou!!")}>Click que aqui também</button>
+            </div>
+            <button onClick={() => {
+                if(true) {
+                    console.log("isso não deveria existir =>")
+                }
+            }}>Clique aqui 2</button>
         </div>
     )
 

f8f27b14a6cd62d28773cf2519e9b2827ddddd18
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-13 14:49:35 -0300

    Previous state

--- 3_AVANÇANDO_NO_REACT/avancando/src/components/ListRender.js
+++ 3_AVANÇANDO_NO_REACT/avancando/src/components/ListRender.js
@@ -4,24 +4,33 @@ const ListRender = () => {
     const [list] = useState(["Kaio", "Pedro", "Matheus"]);
     
     //Simulação banco de dados
-    const [users] = useState([
+    const [users, setUsers] = useState([
         { id: 1, name: "Kaio", age: 31},
-        { id: 23152334, name: "João", age: 28},
-        { id: 823741923, name: "Karen", age: 30},
-    ]);
+        { id: 2, name: "João", age: 28},
+        { id: 3, name: "Karen", age: 30},
+    ])
+
+    const deleteRandom = () => {
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((users) => randomNumber !== users.id)
+        });
+    }
 
     return (
     <div>
         <ul>
             {list.map((item, i) => (
                 <li key={i}>{item}</li>
-            ))};
+            ))}
         </ul>
         <ul>
             {users.map((user) => (
                 <li key={user.id}>{user.name} - {user.age}</li>
-            ))};
+            ))}
         </ul>
+        <button onClick={deleteRandom}>Delete random user</button>
     </div>
   )
 }

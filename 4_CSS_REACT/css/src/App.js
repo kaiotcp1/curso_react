1a154627d06e013406dcd0135f4d44644e33259a
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-22 18:00:50 -0300

    Css de Component

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
+      {/* CSS Global */}
     <h1>Trabalhando com CSS</h1>
+    {/* CSS de Component */}
+    <MyComponent/>
+    <p>Este parágrafo é do App js</p>
+    <p className="my-comp-paragraph">Este tbm é do component</p>
     </div>
   );
 }
---
+++ 4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MyComponent.css";
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>Css de component</h1>
+        <p>Este é o parágrafo do component</p>
+    </div>
+  )
+}
+
+export default MyComponent

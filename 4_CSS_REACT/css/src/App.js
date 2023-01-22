d7e85219c35f76f5342bf76f6debab7c6b01eaf1
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-22 18:23:53 -0300

    Css Modules

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
 
@@ -20,6 +21,8 @@ function App() {
     <h2 style={n > 10 ? ({color: "purple"}) : ({color: "green"})}>Css Inline dinâmico</h2>
     {/* Classe dinâmica */}
     <h2 className={redTitle ? "red-title" : "title"  }>Este título vai ter classe dinâmica</h2>
+    {/* Css Modules */}
+    <Title/>
     </div>
   );
 }

2d9a0d9d2129de23a5a22e49d23b43017f4e4f01
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-22 18:10:40 -0300

    Css Inline Dinâmico

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import MyComponent from './components/MyComponent';
 
 function App() {
+
+  const n = 15;
+
   return (
     <div className="App">
       {/* CSS Global */}
@@ -12,6 +15,8 @@ function App() {
     <p className="my-comp-paragraph">Este tbm é do component</p>
     {/* Inline CSS */}
     <p style={{color: "red", borderTop: "2px solid red", padding: "25px"}}>Este elemento foi estilizado de forma inline</p>
+    {/* CSS inline dinâmico */}
+    <h2 style={n > 10 ? ({color: "purple"}) : ({color: "green"})}>Css Inline dinâmico</h2>
     </div>
   );
 }

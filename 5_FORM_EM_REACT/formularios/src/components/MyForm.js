65a46441b7d96f5ec790b57c0ec1c933e329ebbd
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-23 15:20:47 -0300

    Gerenciamento de dados de input (State inline)

--- 5_FORM_EM_REACT/formularios/src/components/MyForm.js
+++ 5_FORM_EM_REACT/formularios/src/components/MyForm.js
@@ -10,7 +10,7 @@ const MyForm = () => {
         setName(e.target.value);
    }
    console.log(name);
-   
+   console.log(email);
    
     return (
         <div>
@@ -23,7 +23,7 @@ const MyForm = () => {
                 {/* 2 - label envolvendo input */}
                 <label>
                     <span>E-mail</span>
-                    <input type="email" name='email' placeholder='Digite o seu e-mail' />
+                    <input type="email" name='email' placeholder='Digite o seu e-mail' onChange={(e) => setEmail(e.target.value)}/>
                 </label>
                 <input type="submit" value="Enviar" />
             </form>

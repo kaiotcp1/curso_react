0e220d6ac5f3ff39d3d4bc328c28b641c4159e2a
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-23 15:45:35 -0300

    Envio de formulário

--- 5_FORM_EM_REACT/formularios/src/components/MyForm.js
+++ 5_FORM_EM_REACT/formularios/src/components/MyForm.js
@@ -9,13 +9,22 @@ const MyForm = () => {
    const handleName = (e) => {
         setName(e.target.value);
    }
-   console.log(name);
-   console.log(email);
+   //console.log(name);
+   //console.log(email);
+
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Enviando form");
+    console.log(name, email);
+   }
+
+
    
     return (
         <div>
+           {/* 5 - envio de form */}
             {/* 1 - criação de form */}
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Nome:</label>
                     <input type="text" name='name' placeholder='Digite seu nome' onChange={handleName} />
@@ -23,6 +32,7 @@ const MyForm = () => {
                 {/* 2 - label envolvendo input */}
                 <label>
                     <span>E-mail</span>
+                   {/* 4 - simplificação de manipulação de state */}
                     <input type="email" name='email' placeholder='Digite o seu e-mail' onChange={(e) => setEmail(e.target.value)}/>
                 </label>
                 <input type="submit" value="Enviar" />

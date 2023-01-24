0cb88e246c3d130ae7263a3af118121ea3a4f9fc
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-24 16:08:07 -0300

    Resgaando dados da API (forma básica)

---
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/products";
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchDate() {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    fetchDate();
+  }, [])
+
+  console.log(products);
+  
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;

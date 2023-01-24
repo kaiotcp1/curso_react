9a4d9fb9d84b2be6198bd80f7bb4d31ced220ac3
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-24 17:59:11 -0300

    Finalizando Modulo

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,81 +1,85 @@
-import './App.css';
+import "./App.css";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-// 4 import custom hook
-import {useFetch} from './hooks/useFetch'
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
 
+// 8 - errar url para mostrar erro
+// "http://localhost:3001/products"
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
-  
-  // 4 custom hook
-  const {data: items} = useFetch(url);
-  console.log(items);
-  
-  
+
+  // 4 - custom hook e 5 - refactor post
+  const { data: items, httpConfig, loading, error } = useFetch(url);
+
   const [name, setName] = useState("");
-  const [price, setprice] = useState("");
+  const [price, setPrice] = useState("");
 
+  // 1 - resgatando dados
+  // useEffect(async () => {
+  //   const res = await fetch("http://localhost:3000/products");
 
+  //   const data = await res.json();
 
+  //   setProducts(data);
+  // }, []);
 
-  // // 1 - resgatando dados
-  // useEffect(() => {
-  //   async function fetchDate() {
-  //     const res = await fetch(url);
-  //     const data = await res.json();
-
-  //     setProducts(data);
-  //   }
-  //   fetchDate();
-  // }, [])
-
-
-
-  // 2 - add de produtos
+  // 2 - add product
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
     const product = {
       name,
       price,
     };
 
+    // const res = await fetch("http://localhost:3000/products", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(product),
-    })
+    // const addedProduct = await res.json();
+
     // 3 - carregamento dinâmico
-    const addedProduct = await res.json();
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
-    setName("");
-    setprice("");
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
 
+    // 5 - refatorar post
+    httpConfig(product, "POST");
+
+    setName("");
+    setPrice("");
   };
 
-
-
-
-
-
-
-
+  /* 9 - desafio */
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE");
+  };
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      {/* 6 - state de loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
       <ul>
-        {items && items.map((product) => (
-          <li key={product.id}>
-            {product.name} - R$: {product.price}
-          </li>
-        ))}
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+              {/* 9 - desafio */}
+              <button onClick={() => handleRemove(product.id)}>Excluir</button>
+            </li>
+          ))}
       </ul>
 
       <div className="add-product">
+        <p>Adicionar produto:</p>
         <form onSubmit={handleSubmit}>
           <label>
             Nome:
@@ -83,7 +87,8 @@ function App() {
               type="text"
               value={name}
               name="name"
-              onChange={(e) => setName(e.target.value)} />
+              onChange={(e) => setName(e.target.value)}
+            />
           </label>
           <label>
             Preço:
@@ -91,12 +96,13 @@ function App() {
               type="number"
               value={price}
               name="price"
-              onChange={(e) => setprice(e.target.value)} />
+              onChange={(e) => setPrice(e.target.value)}
+            />
           </label>
-          <input type="submit" value="Criar" />
+          {/* 7 - state de loading no post */}
+          {loading ? <p>Aguarde!</p> : <input type="submit" value="Criar" />}
         </form>
       </div>
-
     </div>
   );
 }

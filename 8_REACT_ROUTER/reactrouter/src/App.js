c4691dee98549b679d4ab86e4a82b2db0cc31788
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-26 16:45:32 -0300

    Nested Routes

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 // 1- config react router
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+// components
+import Navbar from './components/Navbar';
+
 // pages
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
-
-// components
-import Navbar from './components/Navbar';
+import Info from './pages/Info';
 
 function App() {
   return (
@@ -20,7 +21,10 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
+          {/* Rota Dinâmica */}
           <Route path='/products/:id' element={<Product />} />
+          {/* Nested route */}
+          <Route path='/products/:id/info' element={<Info />} />
         </Routes>
       </BrowserRouter>
     </div>
--- 8_REACT_ROUTER/reactrouter/src/pages/Product.js
+++ 8_REACT_ROUTER/reactrouter/src/pages/Product.js
@@ -1,29 +1,31 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { useFetch } from '../hooks/useFetch'
 
 
 const Product = () => {
     // Rota dinâmica
-    const {id} = useParams()
+    const { id } = useParams()
 
     // Carregamento dado individual
     const url = "http://localhost:3000/products/" + id
-    const {data: product, loading, error} = useFetch(url);
+    const { data: product, loading, error } = useFetch(url);
 
-  return (
-    <div>
-        <p>ID do produto: {id}</p>
-        {error & <p>Ocorreu um erro...</p>}
-        {loading && <p>Carregando...</p>}
-        {product && (
-            <div> 
-                <h1>{product.name}</h1>
-                <p>R$: {product.price}</p>
-            </div>
-        )}
-    </div>
-  )
+    return (
+        <div>
+            <p>ID do produto: {id}</p>
+            {error & <p>Ocorreu um erro...</p>}
+            {loading && <p>Carregando...</p>}
+            {product && (
+                <div>
+                    <h1>{product.name}</h1>
+                    <p>R$: {product.price}</p>
+                    {/* nested routes */}
+                    <Link to={`/products/${product.id}/info`}>Mais informações</Link>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Product

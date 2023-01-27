0ca9ac28e966a6d291fe903408901f52864baa7b
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-01-26 21:18:12 -0300

    Search params + Redirecionamento de URL

--- 8_REACT_ROUTER/reactrouter/src/App.js
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 // 1- config react router
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 // components
 import Navbar from './components/Navbar';
@@ -12,12 +12,17 @@ import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound'
+import SearchForm from './components/SearchForm';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        {/* Search */}
+        <SearchForm />
+
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
@@ -26,8 +31,12 @@ function App() {
           <Route path='/products/:id' element={<Product />} />
           {/* Nested Route */}
           <Route path='/products/:id/info' element={<Info />} />
+          {/* Search */}
+          <Route path='/search' element={<Search />} />
+          {/* Redirect */}
+          <Route path='/company' element={<Navigate to="/about" />} />
           {/* No Match Route */}
-          <Route path='*' element={<NotFound />}></Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
